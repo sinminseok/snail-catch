60612c0847c2130cef5f38a1adcff5696a2fc0b6
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-05-14 20:09:05 +0900

    docs : 리드미 작성

---
+++ src/main/java/com/snailcatch/snailcatch/SnailcatchApplication.java
@@ -0,0 +1,13 @@
+package com.snailcatch.snailcatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailcatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailcatchApplication.class, args);
+	}
+
+}
