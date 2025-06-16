19420d72ad3cd3dd6d462888aed7d80cb57a5cfe
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-06-17 04:34:31 +0900

    fix : EXPLAIN 상수 공백 추가

--- src/main/java/com/snailcatch/snailcatch/global/constants/SqlConstants.java
+++ src/main/java/com/snailcatch/snailcatch/global/constants/SqlConstants.java
@@ -2,7 +2,7 @@ package com.snailcatch.snailcatch.global.constants;
 
 public class SqlConstants {
     private SqlConstants() {}
-    public static final String EXPLAIN = "EXPLAIN";
+    public static final String EXPLAIN = "EXPLAIN ";
     public static final String SELECT_QUERY_PREFIX = "select";
     public static final String[] EXPLAIN_COLUMNS = {
             "id", "select_type", "table", "partitions", "type", "possible_keys",
